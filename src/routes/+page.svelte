bbc1c23ce9b7117a6adda28a8418a33e30ddf4e7
Brent Blake <77595164+brentvblake@users.noreply.github.com>
2023-05-28 12:06:47 +0200

    adjusted .git folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import "../app.css";
+</script>
+<main class="flex">
+	<section class="w-1/4 flex-1 bg-gray-900 p-4 text-gray-800 dark:text-white fixed h-screen overflow-y-auto">
+		<div>
+			<h2 class="text-2xl font-semibold capitalize">Brent Blake</h2>
+			<p class="text-lg">Walvis Bay, Namibia</p>
+			<p class="text-lg">github.com/brentvblake</p>
+			<p class="text-lg">brent99blake@gmail.com</p>
+			<p class="text-lg">linkedin.com/in/brentvblake</p>
+			<!-- Add more personal information as needed -->
+		</div>
+	</section>
+	
+	
+	<section class="w-3/4 ml-[25%] bg-white dark:bg-gray-900">
+    <div class="container px-6 py-10 mx-auto">
+        <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Portfolio
+        </h1>
+
+        <p class="mt-4 text-center text-gray-500 dark:text-gray-300">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus
+        </p>
+
+
+        <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+            <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+                style="background-image:url('https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80')">
+                <div
+                    class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+                    <h2 class="mt-4 text-xl font-semibold text-white capitalize">Best website collections</h2>
+                    <p class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Website</p>
+                </div>
+            </div>
+
+            <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+                style="background-image:url('https://images.unsplash.com/photo-1621609764180-2ca554a9d6f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80')">
+                <div
+                    class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+                    <h2 class="mt-4 text-xl font-semibold text-white capitalize">Block of Ui kit collections</h2>
+                    <p class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Ui kit</p>
+                </div>
+            </div>
+
+            <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+                style="background-image:url('https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')">
+                <div
+                    class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+                    <h2 class="mt-4 text-xl font-semibold text-white capitalize">Ton’s of mobile mockup</h2>
+                    <p class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Mockups</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</main>
