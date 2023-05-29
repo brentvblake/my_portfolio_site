dcdac5a1f8bb11f8be20310ec976b037dc986aa8
Brent Blake <77595164+brentvblake@users.noreply.github.com>
2023-05-29 12:57:21 +0200

    trying to make dynamic projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,41 @@
 <script>
 	import "../app.css";
+
+    var projects = [
+        {
+            "title": "Fantasy F1 Data Visualised",
+            "description": "This is project 1",
+            "image": "f1data.png",
+            "link": "https://github.com/brentvblake/f1_2022_fantasy_data_visualized"
+        },
+    ];
+
+    function allProjects(){
+        var projectList = [];
+        for (var i = 0; i < projects.length; i++) {
+            projectList.push(
+                <div class="overflow-hidden bg-center bg-cover rounded-lg cursor-pointer h-96 group"
+                    style={"background-image:url("+projects[i].image +")"}>
+                    <a href={projects[i].link} target="_blank">
+                        <div
+                            class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+                            <h2 class="mt-4 text-xl font-semibold text-white capitalize">{projects[i].title}</h2>
+                            <p class="mt-2 text-lg tracking-wider text-purple-500 uppercase ">{projects[i].description}</p>
+                        </div>
+                    </a>
+                </div>
+            )
+        }
+        return projectList;
+    }
 </script>
 
 <body class="bg-radial-gradient">
     
 
-<main class="flex">
-	<section class="w-2/5 flex-1 p-4 text-white fixed h-screen overflow-y-auto">
-		<div class="p-10">
+<main class="flex flex-col items-center md:flex-row">
+	<section class="w-full md:w-2/5 p-4 text-white h-screen overflow-y-auto md:fixed top-0">
+		<div class="mt-10 md:mt-0 p-10 md:p-10">
 			<h2 class="text-2xl font-semibold capitalize">Brent Blake</h2>
 			<p class="text-lg">Walvis Bay, Namibia</p>
 			<p class="text-lg">github.com/brentvblake</p>
@@ -15,25 +43,31 @@
 			<p class="text-lg">linkedin.com/in/brentvblake</p>
 			<!-- Add more personal information as needed -->
 		</div>
-        <div class = "fixed bottom-4 left-4 bg-gray-800 p-4 rounded-lg">
-            <div class="flex items-center space-x-4">
-                <img src="/dino.jpeg" alt="Brent" class="w-10 h-10 rounded-full ring-4 ring-purple-500">
-            
-                <a href="https://github.com/brentvblake" target="_blank" rel="noopener noreferrer" class="flex items-center space-x-2 text-white hover:text-blue-400">
-                    <img src="/github.svg" alt="GitHub logo" class="w-10 h-10 text-white fill=currentColor">
-                    <span>Github</span>
-                </a>
-            
-                <a href="https://linkedin.com/in/brentvblake" target="_blank" rel="noopener noreferrer" class="flex items-center space-x-2 text-white hover:text-blue-400">
-                    <img src="/linkedin.svg" alt="LinkedIn logo" class="w-10 h-10 text-white fill-currentColor ">
-                    <span>LinkedIn</span>
-                </a>
+        <div class="md:fixed bottom-4 left-1/2 md:left-[20%] md:transform md:-translate-x-1/2 flex items-center space-x-4">
+            <img src="/dino.jpeg" alt="Brent" class="w-10 h-10 rounded-full ring-4 ring-purple-500">
+            <div class="flex space-x-4">
+                <div class="flex items-center space-x-2 text-white bg-gray-800 p-4 rounded-lg hover:bg-purple-500 transition-all duration-200">
+                    <a href="https://github.com/brentvblake" target="_blank" rel="noopener noreferrer" class="flex items-center space-x-2">
+                        <img src="/github.svg" alt="GitHub logo" class="w-6 h-6 text-white fill=currentColor">
+                        <span>Github</span>
+                        <img src="/share.svg" alt="share icon" class="w-6 h-6 text-white fill=currentColor">
+                    </a>
+                </div>
+                <div class="flex items-center space-x-2 text-white bg-gray-800 p-4 rounded-lg hover:bg-purple-500 transition-all duration-200">
+                    <a href="https://linkedin.com/in/brentvblake" target="_blank" rel="noopener noreferrer" class="flex items-center space-x-2">
+                        <img src="/linkedin.svg" alt="LinkedIn logo" class="w-6 h-6 text-white fill=currentColor">
+                        <span>LinkedIn</span>
+                        <img src="/share.svg" alt="share icon" class="w-6 h-6 text-white fill=currentColor">
+                    </a>
+                </div>
             </div>
         </div>
+        
+        
 	</section>
 	
 	
-	<section class="w-3/5 ml-[40%]">
+	<section class="w-full md:w-3/5 md:ml-[40%] mt-10 md:mt-0">
     <div class="container px-6 py-10 mx-auto">
         <h1 class="text-2xl font-semibold text-center text-white">Portfolio
         </h1>
@@ -44,31 +78,15 @@
 
 
         <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-1 xl:grid-cols-1">
-            <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
-                style="background-image:url('https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80')">
-                <div
-                    class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-                    <h2 class="mt-4 text-xl font-semibold text-white capitalize">Best website collections</h2>
-                    <p class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Website</p>
-                </div>
-            </div>
-
-            <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
-                style="background-image:url('https://images.unsplash.com/photo-1621609764180-2ca554a9d6f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80')">
-                <div
-                    class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-                    <h2 class="mt-4 text-xl font-semibold text-white capitalize">Block of Ui kit collections</h2>
-                    <p class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Ui kit</p>
-                </div>
-            </div>
-
-            <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
-                style="background-image:url('https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')">
-                <div
-                    class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-                    <h2 class="mt-4 text-xl font-semibold text-white capitalize">Ton’s of mobile mockup</h2>
-                    <p class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Mockups</p>
-                </div>
+            <div class="overflow-hidden bg-center bg-cover rounded-lg cursor-pointer h-96 group"
+                style={"background-image:url("+projects[0].image +")"}>
+                <a href={projects[0].link} target="_blank">
+                    <div
+                        class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+                        <h2 class="mt-4 text-xl font-semibold text-white capitalize">{projects[0].title}</h2>
+                        <p class="mt-2 text-lg tracking-wider text-purple-500 uppercase ">{projects[0].description}</p>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
