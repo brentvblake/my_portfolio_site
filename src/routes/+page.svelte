7ea84b898494ba58297bd16afa5ceb84142cc289
Brent Blake <77595164+brentvblake@users.noreply.github.com>
2023-05-30 09:00:01 +0200

    fixed projects display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,31 +3,36 @@
 
     var projects = [
         {
+            "title": "Personal Protection Equipment (PPE) Vending App",
+            "description": "A showcase of a PPE vending app I created, providing easy access to personal protective equipment.",
+            "image": "vending.png",
+            "link": "https://github.com/brentvblake/PPE_Vending_App"
+        },
+        {
+            "title": "Evolution of Music Taste",
+            "description": "Analysing Popular Music Trends Over Time",
+            "image": "songs.png",
+            "link": "https://github.com/brentvblake/Popular-Music-Evolution-analysed"
+        },
+        {
+            "title": "WhatsApp Chat Data Visualizer",
+            "description": "Transforms exported WhatsApp chat data into insightful visualizations",
+            "image": "whatsapp.png",
+            "link": "https://github.com/brentvblake/WhatsApp-Chat-Analysis-and-Data-Visuals"
+        },
+        {
             "title": "Fantasy F1 Data Visualised",
             "description": "This is project 1",
             "image": "f1data.png",
             "link": "https://github.com/brentvblake/f1_2022_fantasy_data_visualized"
         },
+        {
+            "title": "This Website",
+            "description": "A personal website to showcase my projects and skills",
+            "image": "site.png",
+            "link": "https://github.com/brentvblake/f1_2022_fantasy_data_visualized"
+        },
     ];
-
-    function allProjects(){
-        var projectList = [];
-        for (var i = 0; i < projects.length; i++) {
-            projectList.push(
-                <div class="overflow-hidden bg-center bg-cover rounded-lg cursor-pointer h-96 group"
-                    style={"background-image:url("+projects[i].image +")"}>
-                    <a href={projects[i].link} target="_blank">
-                        <div
-                            class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-                            <h2 class="mt-4 text-xl font-semibold text-white capitalize">{projects[i].title}</h2>
-                            <p class="mt-2 text-lg tracking-wider text-purple-500 uppercase ">{projects[i].description}</p>
-                        </div>
-                    </a>
-                </div>
-            )
-        }
-        return projectList;
-    }
 </script>
 
 <body class="bg-radial-gradient">
@@ -69,25 +74,21 @@
 	
 	<section class="w-full md:w-3/5 md:ml-[40%] mt-10 md:mt-0">
     <div class="container px-6 py-10 mx-auto">
-        <h1 class="text-2xl font-semibold text-center text-white">Portfolio
+        <h1 class="text-4xl font-semibold text-center text-white">Projects
         </h1>
-
-        <p class="mt-4 text-center text-gray-300">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus
-        </p>
-
-
         <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-1 xl:grid-cols-1">
+            {#each projects as project}
             <div class="overflow-hidden bg-center bg-cover rounded-lg cursor-pointer h-96 group"
-                style={"background-image:url("+projects[0].image +")"}>
-                <a href={projects[0].link} target="_blank">
+                style={"background-image:url("+project.image +")"}>
+                <a href={project.link} target="_blank">
                     <div
                         class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-                        <h2 class="mt-4 text-xl font-semibold text-white capitalize">{projects[0].title}</h2>
-                        <p class="mt-2 text-lg tracking-wider text-purple-500 uppercase ">{projects[0].description}</p>
+                        <h2 class="mt-4 text-xl font-semibold text-white capitalize">{project.title}</h2>
+                        <p class="mt-2 text-lg tracking-wider text-purple-500 uppercase ">{project.description}</p>
                     </div>
                 </a>
             </div>
+            {/each}
         </div>
     </div>
 </section>
